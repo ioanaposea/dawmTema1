18bdbefbf363de09ebbb1d431b9e4685b58bf387
ioanaposea <yoa.pos02@gmail.com>
2023-03-28 20:15:27 +0300

    Added dependency injection

--- dawmTema1/Program.cs
+++ dawmTema1/Program.cs
@@ -1,11 +1,15 @@
+using dawmTema1.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+Dependencies.Inject(builder);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
---
+++ dawmTema1/Settings/Dependencies.cs
@@ -0,0 +1,28 @@
+﻿using dawmTema1.Repositories;
+using dawmTema1.Services;
+
+namespace dawmTema1.Settings
+{
+    public static class Dependencies
+    {
+        public static void Inject(WebApplicationBuilder applicationBuilder)
+        {
+            applicationBuilder.Services.AddControllers();
+            applicationBuilder.Services.AddSwaggerGen();
+
+            AddRepositories(applicationBuilder.Services);
+            AddServices(applicationBuilder.Services);
+        }
+
+        private static void AddServices(IServiceCollection services)
+        {
+            services.AddScoped<ClientService>();
+        }
+
+        private static void AddRepositories(IServiceCollection services)
+        {
+            services.AddScoped<ClientsRepository>();
+        }
+    }
+
+}
